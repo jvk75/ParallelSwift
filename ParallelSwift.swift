2aae8acdea3273feef6a5ac977bb23919fbaef1d
Jari Kalinainen <jari@klubitii.com>
2018-01-29 23:14:20 +0200

    Create ParallelSwift.swift

---
+++ ParallelSwift.swift
@@ -0,0 +1,46 @@
+//
+//  ParallelSwift.swift
+//
+//  Created by Jari Kalinainen on 29.01.18.
+//  Copyright © 2018 Jari Kalinainen. All rights reserved.
+//
+// https://github.com/jvk75/ParallelSwift
+
+import Foundation
+
+class ParallelSwift {
+    private var phases: [(@escaping () -> ()) -> ()] = []
+    private let barrier = DispatchQueue(label: "jk.paraller.fi", attributes: .concurrent)
+    
+    private var numberOfPhases: Int = 0
+    
+    private var complete: (() -> ())?
+    
+    public func addPhase(_ phase: @escaping ( @escaping () -> () ) -> ())  {
+        phases.append(phase)
+    }
+    
+    public func execute(_ complete: @escaping () -> ()) {
+        self.numberOfPhases = phases.count
+        self.complete = complete
+        self.phases.forEach({ phase in
+            self.barrier.sync(flags: .barrier) {
+                phase(done)
+            }
+        })
+    }
+    
+    private func done() -> () {
+        numberOfPhases -= 1
+        if numberOfPhases == 0 {
+            complete?()
+            reset()
+        }
+    }
+    
+    private func reset() {
+        phases = []
+        numberOfPhases = 0
+        complete = nil
+    }
+}
